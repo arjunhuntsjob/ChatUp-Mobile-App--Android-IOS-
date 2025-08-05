a7f223ca4df38de0480619cdd380e8d7229f4f34
Arjun <arjunkanthvenkatagiri@gmail.com>
2025-08-05 22:45:07 +0530

    ios notifications added

--- ios/ChatUp/AppDelegate.swift
+++ ios/ChatUp/AppDelegate.swift
@@ -1,10 +1,61 @@
+// import UIKit
+// import React
+// import React_RCTAppDelegate
+// import ReactAppDependencyProvider
+
+// @main
+// class AppDelegate: UIResponder, UIApplicationDelegate {
+//   var window: UIWindow?
+
+//   var reactNativeDelegate: ReactNativeDelegate?
+//   var reactNativeFactory: RCTReactNativeFactory?
+
+//   func application(
+//     _ application: UIApplication,
+//     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+//   ) -> Bool {
+//     let delegate = ReactNativeDelegate()
+//     let factory = RCTReactNativeFactory(delegate: delegate)
+//     delegate.dependencyProvider = RCTAppDependencyProvider()
+
+//     reactNativeDelegate = delegate
+//     reactNativeFactory = factory
+
+//     window = UIWindow(frame: UIScreen.main.bounds)
+
+//     factory.startReactNative(
+//       withModuleName: "ChatUp",
+//       in: window,
+//       launchOptions: launchOptions
+//     )
+
+//     return true
+//   }
+// }
+
+// class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
+//   override func sourceURL(for bridge: RCTBridge) -> URL? {
+//     self.bundleURL()
+//   }
+
+//   override func bundleURL() -> URL? {
+// #if DEBUG
+//     RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+// #else
+//     Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+// #endif
+//   }
+// }
+
+
 import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
   var window: UIWindow?
 
   var reactNativeDelegate: ReactNativeDelegate?
@@ -29,7 +80,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       launchOptions: launchOptions
     )
 
+    // Add notification center delegate
+    UNUserNotificationCenter.current().delegate = self
+
     return true
+  }
+
+  // Handle notification when app is in foreground
+  func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    completionHandler([.alert, .badge, .sound])
+  }
+  
+  // Handle notification tap
+  func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+    completionHandler()
   }
 }
 
